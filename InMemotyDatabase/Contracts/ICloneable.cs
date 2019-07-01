77c8c12a07326d1ea5b599564576f2943cfa133a
Jivko <jivkokirishev@gmail.com>
2019-07-01 23:10:03 +0300

    Added Clonable and Nameable interfaces which will be used for creating components.

---
+++ InMemotyDatabase/Contracts/ICloneable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InMemoryDatabase.Contracts
+{
+    public interface ICloneable<T>
+    {
+        T Clone();
+    }
+}
