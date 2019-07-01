771a49caa6f24f1610d674fa237d860a54fc666d
Jivko <jivkokirishev@gmail.com>
2019-07-01 23:16:53 +0300

    New component created - Cell. Also enum class is added to store the types of the cells. Utility class ValueTypeChecker created.

---
+++ InMemotyDatabase/Utilities/ValueTypeChecker.cs
@@ -0,0 +1,35 @@
+﻿using InMemoryDatabase.Enums;
+using System;
+
+namespace InMemoryDatabase.Utilies
+{
+    public class ValueTypeChecker
+    {
+        public bool Check(EnumType type, string value)
+        {
+            if (type == EnumType.Integer)
+            {
+                int unusedValue;
+                bool isInteger = int.TryParse(value, out unusedValue);
+
+                return isInteger;
+            }
+            else if (type == EnumType.Real)
+            {
+                double unusedValue;
+                bool isReal = double.TryParse(value, out unusedValue);
+
+                return isReal;
+            }
+            else if (type == EnumType.String)
+            {
+                return true;
+            }
+            else
+            {
+                string exceptionMessage = "Unsupported type.";
+                throw new ArgumentException(exceptionMessage);
+            }
+        }
+    }
+}
