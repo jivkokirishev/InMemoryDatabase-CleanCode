5878448be28bfbe8530218866590523bf30a87c5
Jivko <jivkokirishev@gmail.com>
2019-07-02 13:29:41 +0300

    Database tranformations added. The aggregate function is not implemented yet.

---
+++ InMemotyDatabase/Managers/DbTransformations.cs
@@ -0,0 +1,42 @@
+﻿using InMemoryDatabase.Components;
+using System;
+using System.Collections.Generic;
+
+namespace InMemoryDatabase.Managers
+{
+    public class DbTransformations
+    {
+        private Database database;
+
+        public DbTransformations(Database database)
+        {
+            this.database = database;
+        }
+
+
+        public IList<string> TableNames()
+        {
+            var names = new List<string>();
+
+            foreach (var table in database.Tables)
+            {
+                names.Add(table.Name);
+            }
+
+            return names;
+        }
+
+        public Cell Agregate(string tableName, string searchColumnName, Cell searchValue,
+            string targetColumnName, OperationType operation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TableCount()
+        {
+            var tableCount = database.TableCount();
+
+            return tableCount;
+        }
+    }
+}
