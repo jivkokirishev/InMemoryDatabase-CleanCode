177e4739ee5eeff8b7592af58a28b8a3582bed15
Jivko <jivkokirishev@gmail.com>
2019-07-02 12:37:06 +0300

    Tests for the Cell functionalities added.

--- InMemoryDatabase.Test/CellTests.cs
+++ InMemoryDatabase.Test/CellTests.cs
@@ -1,9 +1,72 @@
+using InMemoryDatabase.Components;
+using InMemoryDatabase.Enums;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
-namespace InMemoryDatabase.Test
+namespace InMemotyDatabase.Test
 {
     [TestClass]
     public class CellTests
     {
+        [TestMethod]
+        public void GetNullValueTest()
+        {
+            //Arrange
+            Cell cell = new Cell(EnumType.Integer);
+
+            //Act  
+            string actual = cell.Value;
+
+            //Assert  
+            string expected = "NULL";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetValueTest()
+        {
+            //Arrange
+            Cell cell = new Cell(EnumType.Integer, "123");
+
+            //Act  
+            string actual = cell.Value;
+
+            //Assert  
+            string expected = "123";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SettingValueThaCanNotBeParsedToTheCellTypeShouldThrowException()
+        {
+            Cell cell = new Cell(EnumType.Integer, "123.506");
+        }
+
+        [TestMethod]
+        public void CheckingValueOfAClonedObject()
+        {
+            Cell cell = new Cell(EnumType.Real, "123.506");
+            Cell clonedCell = cell.Clone();
+
+            clonedCell.Value = "43.345";
+
+            string actual = cell.Value;
+            string expected = "123.506";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckingIsNullOfAClonedObject()
+        {
+            Cell cell = new Cell(EnumType.Real);
+            Cell clonedCell = cell.Clone();
+
+            clonedCell.Value = "43.345";
+
+            string actual = cell.Value;
+            string expected = "NULL";
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
